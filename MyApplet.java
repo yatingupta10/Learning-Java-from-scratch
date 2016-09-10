8be720041e29a595df857f0a4a7d08f0889c5f35
yatingupta10 <yatingupta12@gmail.com>
2016-09-10 18:34:24 +0530

    My First Applet

---
+++ MyApplet.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code="MyApplet.class" height=300 width=100>
+</applet> */
+public class MyApplet extends Applet{
+	public void paint(Graphics g){
+		g.drawString("Hello World!",100,100);
+	}
+}
