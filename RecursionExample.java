7bb50ef67dceab72a9738500cc9e76eec3847cca
yatingupta10 <yatingupta12@gmail.com>
2016-10-22 21:29:44 +0530

    Implemented Recursion in Java

---
+++ RecursionExample.java
@@ -0,0 +1,13 @@
+public class RecursionExample {  
+
+	static int factorial(int n){      
+		  if (n == 1)      
+			return 1;      
+		  else      
+			return(n * factorial(n-1));      
+	}      
+  
+	public static void main(String[] args) {  
+		System.out.println("Factorial of 5 is: "+factorial(5));  
+	}  
+}  
